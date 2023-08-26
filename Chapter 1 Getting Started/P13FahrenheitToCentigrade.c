28b7704d11f58cc555fb5bdab692d2f794ecdb8b
Vandana <av102451293@gmail.com>
2023-08-26 16:14:05 +0530

    P14RectCirAreaPeri Added By Vandana @YouTube/DearCoding

--- Chapter 1 Getting Started/P13FahrenheitToCentigrade.c
+++ Chapter 1 Getting Started/P13FahrenheitToCentigrade.c
@@ -1,3 +1,10 @@
+/* C Programming By Dear Coding
+ * Author: Vandana @Dear-Coding 💜
+ * https://www.youtube.com/@DearCoding */
+
+💜 Watch All Chapters PLAYLIST On YouTube 💜  
+https://www.youtube.com/playlist?list=PLdBju4Bv7C3B2ZSf2gzE0Epd3d668VIGP 
+
 #include<stdio.h>
 
 int main() {
@@ -13,3 +20,11 @@ int main() {
         printf("Temperature of the City in Degree Centigrade is %.2f.", degC);
         return 0;
 }
+
+// You have better code??
+// Don't keep it to just yourself, 
+// Share it wtih the Coding World.
+PUSH YOUR CHNAGES HERE!!💜💜💜
+// The chnages will be visible on your profile.
+IT WILL BE VERY HELPFUL IN YOUR CODING INTERVIEW.
+// Also, Knowledge only increases by sharing.
---
+++ Chapter 1 Getting Started/P14RectCirAreaPeri.c
@@ -0,0 +1,36 @@
+/* C Programming By Dear Coding
+ * Author: Vandana @Dear-Coding 💜
+ * https://www.youtube.com/@DearCoding */
+
+💜 Watch All Chapters PLAYLIST On YouTube 💜  
+https://www.youtube.com/playlist?list=PLdBju4Bv7C3B2ZSf2gzE0Epd3d668VIGP 
+
+#include<stdio.h>
+
+int main() {
+
+        float length;
+        float breadth;
+        float radius;
+        const float PI = 3.14159265;
+
+        printf("Enter the Length and Breadth of a Rectangle: ");
+        scanf("%f %f", &length, &breadth);
+        printf("Area of the Rectangle = %.2f\n", length*breadth);
+        printf("Perimeter of the Rectangle = %.2f\n\n", 2*(length+breadth));
+
+        printf("Enter the Radius of a Circle: ");
+        scanf("%f", &radius);
+        printf("Area of the Circle = %.2f\n", PI*radius*radius);
+        printf("Circumference of the Circle = %.2f", 2*PI*radius);
+
+    return 0;
+}
+
+// You have better code??
+// Don't keep it to just yourself, 
+// Share it wtih the Coding World.
+PUSH YOUR CHNAGES HERE!!💜💜💜
+// The chnages will be visible on your profile.
+IT WILL BE VERY HELPFUL IN YOUR CODING INTERVIEW.
+// Also, Knowledge only increases by sharing.
