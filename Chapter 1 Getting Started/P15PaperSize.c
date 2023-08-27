8e380aecbff3f6291c76fa4843738dcccdd7ee91
Vandana <av102451293@gmail.com>
2023-08-27 17:20:52 +0530

    P15PaperSize Added By Vandana @YouTube/DearCoding

---
+++ Chapter 1 Getting Started/P15PaperSize.c
@@ -0,0 +1,34 @@
+/* C Programming By Dear Coding
+ * Author: Vandana @Dear-Coding 💜
+ * https://www.youtube.com/@DearCoding */
+
+💜 Watch All Chapters PLAYLIST On YouTube 💜  
+https://www.youtube.com/playlist?list=PLdBju4Bv7C3B2ZSf2gzE0Epd3d668VIGP 
+
+#include<stdio.h>
+
+int main() {
+
+        int length = 1189;
+        int breadth = 841;
+
+        printf("Dimensions of Paper of Size A0: %d mm x %d mm\n", length, breadth);
+        printf("Dimensions of Paper of Size A1: %d mm x %d mm\n", breadth, length/2);
+        printf("Dimensions of Paper of Size A2: %d mm x %d mm\n", length/2, breadth/2);
+        printf("Dimensions of Paper of Size A3: %d mm x %d mm\n", breadth/2, length/4);
+        printf("Dimensions of Paper of Size A4: %d mm x %d mm\n", length/4, breadth/4);
+        printf("Dimensions of Paper of Size A5: %d mm x %d mm\n", breadth/4, length/8);
+        printf("Dimensions of Paper of Size A6: %d mm x %d mm\n", length/8, breadth/8);
+        printf("Dimensions of Paper of Size A7: %d mm x %d mm\n", breadth/8, length/16);
+        printf("Dimensions of Paper of Size A8: %d mm x %d mm\n", length/16, breadth/16);
+
+    return 0;
+}
+
+// You have better code??
+// Don't keep it to just yourself, 
+// Share it wtih the Coding World.
+PUSH YOUR CHNAGES HERE!!💜💜💜
+// The chnages will be visible on your profile.
+IT WILL BE VERY HELPFUL IN YOUR CODING INTERVIEW.
+// Also, Knowledge only increases by sharing.
