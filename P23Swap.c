27eafd987a1250202750a5eb76e5d41e7a573199
Vandana <av102451293@gmail.com>
2023-09-04 17:15:03 +0530

    P23Swap Added By Vandana @YouTube/DearCoding

---
+++ P23Swap.c
@@ -0,0 +1,37 @@
+/* C Programming By Dear Coding
+ * Author: Vandana @Dear-Coding 💜
+ * https://www.youtube.com/@DearCoding */
+
+💜 Watch All Chapters PLAYLIST On YouTube 💜  
+https://www.youtube.com/playlist?list=PLdBju4Bv7C3B2ZSf2gzE0Epd3d668VIGP 
+
+#include <stdio.h>
+
+int main() {
+
+        int C, D, temp;
+
+        // Read values for C and D
+        printf("Enter value for C: ");
+        scanf("%d", &C);
+        printf("Enter value for D: ");
+        scanf("%d", &D);
+
+        // Interchange the values of C and D
+        temp = C;
+        C = D;
+        D = temp;
+
+        // Print the values of c and d after swapping
+        printf("After swapping: C = %d, D = %d", C, D);
+
+  return 0;
+}
+
+// You have better code??
+// Don't keep it to just yourself, 
+// Share it wtih the Coding World.
+PUSH YOUR CHNAGES HERE!!💜💜💜
+// The chnages will be visible on your profile.
+IT WILL BE VERY HELPFUL IN YOUR CODING INTERVIEW.
+// Also, Knowledge only increases by sharing.
