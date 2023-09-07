9ebe8e0ab95d84a89f685c9d10a035a03961861e
Vandana <av102451293@gmail.com>
2023-09-07 16:34:00 +0530

    P26CPSP Added By Vandana @YouTube/DearCoding

---
+++ Chapter 2 C Instructions/P26CPSP.c
@@ -0,0 +1,40 @@
+/* C Programming By Dear Coding
+ * Author: Vandana @Dear-Coding 💜
+ * https://www.youtube.com/@DearCoding */
+
+💜 Watch All Chapters PLAYLIST On YouTube 💜  
+https://www.youtube.com/playlist?list=PLdBju4Bv7C3B2ZSf2gzE0Epd3d668VIGP 
+
+#include <stdio.h>
+
+int main() {
+    
+    float costPrice;
+    float sellingPrice;
+
+    printf("Enter the cost price: ");
+    scanf("%f", &costPrice);
+
+    printf("Enter the selling price: ");
+    scanf("%f", &sellingPrice);
+
+    if (sellingPrice > costPrice)
+        printf("Profit: %.2f\n", sellingPrice - costPrice);
+    else {
+        if (sellingPrice < costPrice)
+            printf("Loss: %.2f\n", costPrice - sellingPrice);
+        else
+            printf("No profit or loss\n");
+    }
+    
+
+    return 0;
+}
+
+// You have better code??
+// Don't keep it to just yourself, 
+// Share it wtih the Coding World.
+PUSH YOUR CHNAGES HERE!!💜💜💜
+// The chnages will be visible on your profile.
+IT WILL BE VERY HELPFUL IN YOUR CODING INTERVIEW.
+// Also, Knowledge only increases by sharing.
