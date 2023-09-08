24c1abe97e75b3741749694cfde031ce1b167668
Vandana <av102451293@gmail.com>
2023-09-08 15:19:46 +0530

    P27EvenOdd Added By Vandana @YouTube/DearCoding

---
+++ Chapter 2 C Instructions/P27EvenOdd.c
@@ -0,0 +1,31 @@
+/* C Programming By Dear Coding
+ * Author: Vandana @Dear-Coding 💜
+ * https://www.youtube.com/@DearCoding */
+
+💜 Watch All Chapters PLAYLIST On YouTube 💜  
+https://www.youtube.com/playlist?list=PLdBju4Bv7C3B2ZSf2gzE0Epd3d668VIGP 
+
+#include <stdio.h>
+
+int main() {
+    
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0)
+        printf("%d is an even number\n", number);
+    else
+        printf("%d is an odd number\n", number);
+
+    return 0;
+}
+
+// You have better code??
+// Don't keep it to just yourself, 
+// Share it wtih the Coding World.
+PUSH YOUR CHNAGES HERE!!💜💜💜
+// The chnages will be visible on your profile.
+IT WILL BE VERY HELPFUL IN YOUR CODING INTERVIEW.
+// Also, Knowledge only increases by sharing.
